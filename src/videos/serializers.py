3c85fa6e8e04042b52317e95cee979c9582ffdbb
paveu <pstepak@gmail.com>
2015-05-23 23:12:54 +0200

    Nesting Foreign keys

--- src/videos/serializers.py
+++ src/videos/serializers.py
@@ -3,12 +3,14 @@ from rest_framework import routers, serializers, viewsets, permissions
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from .models import Video, Category
+from comments.serializers import CommentSerializer
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Category
         fields = [
                     'id',
+                    'url',
                     'slug',
                     'title',
                     'description',
@@ -24,14 +26,17 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
 
 class VideoSerializer(serializers.HyperlinkedModelSerializer):
+    category = CategorySerializer(many=False, read_only=True)
+    comment_set = CommentSerializer(many=True, read_only=True)
 #     category_url = serializers.CharField(source='category.get_absolute_url', read_only=True)
-    category_title = serializers.CharField(source='category.title', read_only=True)
-    category_image = serializers.CharField(source='category.get_image_url', read_only=True)
+#     category_title = serializers.CharField(source='category.title', read_only=True)
+#     category_image = serializers.CharField(source='category.get_image_url', read_only=True)
 #     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
     class Meta:
         model = Video
         fields = [
                     'id',
+                    'url',
                     'title',
                     'embed_code',
                     'share_message',
@@ -39,8 +44,8 @@ class VideoSerializer(serializers.HyperlinkedModelSerializer):
                     'slug',
                     'timestamp',
                     'category', # taken from serialized Category class
-                    'category_title',
-                    'category_image',
+#                     'category_title',
+#                     'category_image',
 #                     'category_url',
                     'comment_set', # _set will show any comments related to pointed video
                    ]
