d1d63b6e42b203a098155a3e725ea20255afd049
paveu <pstepak@gmail.com>
2015-05-23 21:46:40 +0200

    added basic foreign key usage

--- src/videos/serializers.py
+++ src/videos/serializers.py
@@ -3,9 +3,13 @@ from rest_framework import routers, serializers, viewsets, permissions
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 
-from .models import Video
+from .models import Video, Category
 
 class VideoSerializer(serializers.HyperlinkedModelSerializer):
+    category_url = serializers.CharField(source='category.get_absolute_url', read_only=True)
+    category_title = serializers.CharField(source='category.title', read_only=True)
+    category_image = serializers.CharField(source='category.get_image_url', read_only=True)
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
     class Meta:
         model = Video
         fields = [
@@ -16,6 +20,10 @@ class VideoSerializer(serializers.HyperlinkedModelSerializer):
                     'order',
                     'slug',
                     'timestamp',
+                    'category',
+                    'category_title',
+                    'category_image',
+                    'category_url',
                    ]
 
 
