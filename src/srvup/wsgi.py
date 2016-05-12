751bf8d2d15c00f08e14226628b13de4971f8d9d
Pawel <pstepak@gmail.com>
2016-05-12 13:10:57 +0000

    changed wsgi

--- src/srvup/wsgi.py
+++ src/srvup/wsgi.py
@@ -5,14 +5,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-# import os
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "srvup.settings")
-
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
-
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "srvup.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+application = get_wsgi_application()
 
-application = Cling(get_wsgi_application())
+
