281f6b3248bc671c247a9976c8a2cae39172c689
Pawel <pstepak@gmail.com>
2016-05-18 14:22:03 +0000

    fixed error with aws db settings

--- src/srvup/settings.py
+++ src/srvup/settings.py
@@ -289,14 +289,15 @@ if os.environ.get("CONFIG_ENV") == 'AWS_ELASTIC_BEANSTALK':
     if 'RDS_DB_NAME' in os.environ:
         DATABASES = {
             'default': {
-                'ENGINE': 'django.db.backends.mysql',
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
                 'NAME': os.environ['RDS_DB_NAME'],
                 'USER': os.environ['RDS_USERNAME'],
                 'PASSWORD': os.environ['RDS_PASSWORD'],
                 'HOST': os.environ['RDS_HOSTNAME'],
                 'PORT': os.environ['RDS_PORT'],
             }
-        } 
+        }
+
 
 
 if os.environ.get("CONFIG_ENV") == 'HEROKU':
