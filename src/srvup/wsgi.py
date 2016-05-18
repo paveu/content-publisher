90463e61ba9d929a26b94d9de2b660a1641cf161
Pawel <pstepak@gmail.com>
2016-05-18 20:26:55 +0000

    playing around with wsgi and eb

--- src/srvup/wsgi.py
+++ src/srvup/wsgi.py
@@ -6,9 +6,23 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import time
+import traceback
+import signal
+import sys
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "srvup.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
 
+try:
+    application = get_wsgi_application()
+    print 'WSGI without exception'
+except Exception:
+    print 'handling WSGI exception'
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
